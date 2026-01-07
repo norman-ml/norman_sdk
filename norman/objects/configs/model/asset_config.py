70fa59d09c2d7f52cdab06ed54d2c6521a5dfad4
ori-small <ori.small@norman-ai.com>
2026-01-07 17:12:58 +0200

    CR fixes: added data modality field to model asset for accessing it through sanitization.

--- norman/objects/configs/model/asset_config.py
+++ norman/objects/configs/model/asset_config.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from typing import Optional, Union
 
 from norman_objects.shared.inputs.input_source import InputSource
+from norman_objects.shared.parameters.data_modality import DataModality
 from pydantic import BaseModel, Field
 
 
@@ -9,3 +10,4 @@ class AssetConfig(BaseModel):
     asset_name: str = Field(description="Human friendly name for the model asset")
     data: Union[bytes, str, Path] = Field(description="Actual asset payload")
     source: Optional[InputSource] = Field(None, description="Where the data is coming from")
+    data_modality: Optional[DataModality] = Field(None, description="The data modality")
